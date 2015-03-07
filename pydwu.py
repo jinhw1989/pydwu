4b09e190a0f46a7a2d8124fd9b35bebf9e5a91c0
jinhw1989 <jinhw1989@gmail.com>
2015-03-06 19:27:13 -0600

    previous work

---
+++ pydwu.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime, timedelta
+import os
+import sys
+import re
+import urllib2
+import tempfile
+import threading
+import time
+import urllib
+# try:
+#     from argparse import ArgumentParser as ArgParser
+# except ImportError:
+#     from optparse import OptionParser as ArgParser
+
+__version__ = '0.0.1'
+
+usage = \
+"""
+usage pydwu [OPTIONS] --start=<YYYY-MM-DD> --end=<YYYY-MM-DD> --location=( ZIP | City | Airport )
+    -h, --help  Display this usage message
+    -w          Folder to store downloadereqdb.wmo=99999d file
+    -z ZIP      Download zip code
+    --version   Display version
+Example:
+    Usage:
+    pydwu --start=2010-01-02 --end-2010-02-01 --location=60616
+"""
+
+def get_history_using_HTTP(START_DATE, END_DATE, AIRPORT):
+    '''
+    Get Historical Weather Data through HTTP
+    '''
+    num_days = (END_DATE - START_DATE).days
+    work_day = START_DATE
+
+    # @TODO: use multi thread to download weather data if possible.
+    for i in range(num_days):
+        y = work_day.year
+        m = "%02d" % work_day.month
+        d = "%02d" % work_day.day
+        address = "http://www.wunderground.com/history/airport/{}/{}/{}/{}/DailyHistory.html?format=1".format(AIRPORT,
+            y, m, d)
+        try:
+            os.mkdir(os.path.join(working_dir, AIRPORT))
+        except OSError:
+            pass
+        filename = os.path.join(
+            working_dir, AIRPORT, "wunderground_{}_{}_{}.csv".format(y, m, d))
+        urllib.urlretrieve(address, filename)
+        outfile = ""
+        with open(filename, "r") as infile:
+            infile.readline()
+            for line in infile:
+                line = line.replace("<br />", "")
+                outfile += line
+        with open(filename, "w") as inputFile:
+            inputFile.write(outfile)
+        work_day = work_day + timedelta(days=1)
+
+def pydwu():
+    """
+    """
+    try:
+        sd = "1991-01-01"
+        ed = "1991-01-31"
+        START_DATE = datetime.strptime(sd, "%Y-%m-%d")
+        END_DATE = datetime.strptime(ed, "%Y-%m-%d")
+        AIRPORT = "KMDW"
+        get_history_using_HTTP(START_DATE, END_DATE, AIRPORT)
+        content = urllib2.urlopen("http://www.whereismyip.com").read()
+        ip_addr = re.search(r'[0-9]+(?:\.[0-9]+){3}', content).group(0)
+        print ip_addr
+        print working_dir
+    except KeyboardInterrupt:
+        print("processing interrupted")
+
+    pass
+
+def main():
+    """
+    start
+    """
+    try:
+        global working_dir
+        working_dir = os.path.dirname(os.path.abspath(__file__))
+        pydwu()
+    except KeyboardInterrupt:
+        print("\nCancelling download processing...")
+
+if __name__ == '__main__':
+    main()
