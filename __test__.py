10b3f04899105aeb8b629c3f0fed58325675d7da
Hongwei Jin <jinhw1989@gmail.com>
2015-07-02 20:30:37 -0700

    setup prepare

--- __test__.py
+++ __test__.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 
-
 import urllib2
-
+import json
+import requests
 content = urllib2.urlopen("http://ipinfo.io/json").read()
 with open("ipinfo.json", "w") as outfile:
     outfile.write(content)
+
+with open("ipinfo.json") as infile:
+    data = json.load(infile)
+    zipcode = data.get("postal")
+print zipcode
 
